ae2696c4a529fbb50bd38224b18db8b5f46701b6
Irving ZC <irvingzc0509@gmail.com>
2024-03-23 13:56:37 -0600

    se preparo el dataset de entrada para trabajar el problema

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,13 +1,20 @@
-entry = """    [D]    
-[N] [C]    
-[Z] [M] [P]"""
+entry = """[D]                     [N] [F]    
+[H] [F]             [L] [J] [H]    
+[R] [H]             [F] [V] [G] [H]
+[Z] [Q]         [Z] [W] [L] [J] [B]
+[S] [W] [H]     [B] [H] [D] [C] [M]
+[P] [R] [S] [G] [J] [J] [W] [Z] [V]
+[W] [B] [V] [F] [G] [T] [T] [T] [P]
+[Q] [V] [C] [H] [P] [Q] [Z] [D] [W]"""
 entry = entry.replace("[", "")
 entry = entry.replace("]", "")
 entry = entry.split("\n")
-entry[0] = entry[0].replace("   ", " 0")
+index = 0
+for index_for in entry:
+    index = index
+    entry[index] = entry[index].replace("    ", " 0")
+    entry[index] = entry[index].replace(" ", "")
+    index = index+1
 
 
-print(len(entry[0]))
-print(len(entry[1]))
-print(len(entry[2]))
 print(entry)
