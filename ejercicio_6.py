e528494d0e4f39f0469e4422efde451eec2c1c11
Irving ZC <irvingzc0509@gmail.com>
2024-03-26 18:02:25 -0600

    se inicio ejercicio 6

---
+++ ejercicio_6.py
@@ -0,0 +1,19 @@
+entry = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+
+entry_list_format = [list for list in entry]
+print(entry_list_format)
+if not(entry_list_format[0]!=entry_list_format[1]!=entry_list_format[2]!=entry_list_format[3]):
+    entry_list_format = entry_list_format.pop[0] 
+    print(entry_list_format[0])
+    print(entry_list_format[1])
+    print(entry_list_format[2])
+    print(entry_list_format[3])
+elif entry_list_format[0]!=entry_list_format[1]!=entry_list_format[2]!=entry_list_format[3]:
+    print("ya")
+print(entry_list_format) 
+
+for search_marker in entry_list_format:
+    print(entry_list_format[0])
+    print(entry_list_format[1])
+    print(entry_list_format[2])
+    print(entry_list_format[3]) 
